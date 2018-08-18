cdbb4e6f150b80290c666a2709f53ad74c68866e
-KaiK- <enriqueoriol@gmail.com>
2018-08-18 17:15:15 +0200

    completed point 2

--- src/app/hello/hello.component.ts
+++ src/app/hello/hello.component.ts
@@ -2,9 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
     selector: 'custom-hello',
-    template: `<p>
-                Hello world!
-              </p>`
+    templateUrl: './hello.component.html'
 })
 export class HelloComponent{
 
