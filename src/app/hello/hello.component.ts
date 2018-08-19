ff6788ce6c131f7cc6e77a4782ada20058d1b205
-KaiK- <enriqueoriol@gmail.com>
2018-08-20 01:52:37 +0200

    completed point 9

--- src/app/hello/hello.component.ts
+++ src/app/hello/hello.component.ts
@@ -9,9 +9,22 @@ export class HelloComponent{
     @Input() name:string;
     @Output() sayHello:EventEmitter<string> = new EventEmitter<string>();
 
+    public namesList:Array<string> = [];
+    public inputName:string = 'Jhon';
+
     constructor(){ }
 
     onClick(){
         this.sayHello.emit('Hello!');
+    }
+
+    addName(){
+        this.namesList.push(this.inputName);
+        console.log(this.namesList);
+    }
+
+    updateInputName(event:any){
+        this.inputName = event.target.value;
+        console.log(this.inputName);
     }
 }
