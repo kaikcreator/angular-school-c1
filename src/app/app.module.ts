0f39d0311f1a219ba2197bd04a66d76a3f55c8bf
-KaiK- <enriqueoriol@gmail.com>
2018-08-20 02:11:57 +0200

    completed point 10

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello/hello.component';
@@ -10,7 +11,8 @@ import { HelloComponent } from './hello/hello.component';
     HelloComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
