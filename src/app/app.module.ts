ce9c19a60f276744401ed65c16824aed0ae83ee2
-KaiK- <enriqueoriol@gmail.com>
2018-08-24 04:02:04 +0200

    completed point 22

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello/hello.component';
 import { UpperCapitalPipe } from './hello/upper-capital.pipe';
+import { LoggerService } from './logger.service';
 
 @NgModule({
   declarations: [
@@ -16,7 +17,7 @@ import { UpperCapitalPipe } from './hello/upper-capital.pipe';
     BrowserModule,
     FormsModule
   ],
-  providers: [],
+  providers: [LoggerService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
